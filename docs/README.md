9fdbd2892ec4f4603f0824e334d99092b02777a2
Mark <markgewd@gmail.com>
2026-08-19 00:15:46 +0800

    Rename chatbot

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Dulio User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Dulio.java
@@ -0,0 +1,10 @@
+public class Dulio {
+    public static void main(String[] args) {
+    String banner = " ____  _   _ _     ___ ___  \n"
+                + "|  _ \\| | | | |   |_ _/ _ \\ \n"
+                + "| | | | | | | |    | | | | |\n"
+                + "| |_| | |_| | |___ | | |_| |\n"
+                + "|____/ \\___/|_____|___\\___/ \n";
+        System.out.println(banner);
+    }
+}
