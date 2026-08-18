2c366647a90b59c9ee09f1bc90d32e4626e5ad16
Mark <markgewd@gmail.com>
2026-08-19 00:23:25 +0800

    Add greeting

--- src/main/java/Dulio.java
+++ src/main/java/Dulio.java
@@ -1,10 +1,16 @@
 public class Dulio {
     public static void main(String[] args) {
-    String banner = " ____  _   _ _     ___ ___  \n"
-                + "|  _ \\| | | | |   |_ _/ _ \\ \n"
-                + "| | | | | | | |    | | | | |\n"
-                + "| |_| | |_| | |___ | | |_| |\n"
-                + "|____/ \\___/|_____|___\\___/ \n";
+        String banner = " ____  _   _ _     ___ ___  \n"
+            + "|  _ \\| | | | |   |_ _/ _ \\ \n"
+            + "| | | | | | | |    | | | | |\n"
+            + "| |_| | |_| | |___ | | |_| |\n"
+            + "|____/ \\___/|_____|___\\___/ \n";
+        System.out.println("____________________________________________________________");
         System.out.println(banner);
+        System.out.println("Hello! I'm Dulio.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
